b5dc6461432cc91db287cd583894b30b042a2bdc
moputra <97260958+YoungMo18@users.noreply.github.com>
2024-11-25 20:40:55 -0800

    finish connecting DB, added report endpoint, fixed front end, added XXS front end

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
+import models from './models.js'
+
 import usersRouter from './routes/users.js';
 import reportsRouter from './routes/reports.js';
 
@@ -19,6 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  req.models = models
+  next()
+})
 
 app.use('/users', usersRouter);
 app.use('/reports', reportsRouter);
